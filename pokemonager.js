62a91ff7f8c31d3eb2c0354feab854923ed430ba
Ian Cameron and Mavroian Florin <ian.kameron+mavflorin@gmail.com>
2019-04-19 14:50:43 +0900

    completed findUnderWeight method

--- pokemonager.js
+++ pokemonager.js
@@ -20,9 +20,23 @@
     // This should return an array of all the Pokemon that are under a particular weight.
 
     findUnderWeight(weight) {
-      // Your code here.
-      // ** LIMIT TO THE FIRST 10 POKEMON
-      // We don't want to make too many unnecessary calls to the Pokemon API
+      let promiseArray = [];
+      for (let i = 1; i <= 10; i++) {
+        promiseArray.push(fetch(`https://pokeapi.co/api/v2/pokemon/${i}`));
+      }
+
+      return Promise.all(promiseArray)
+        .then((responseArray) => {
+          let newArray = responseArray.map((response) => {
+            return response.json();
+          });
+          return Promise.all(newArray);
+        })
+        .then((pokemons) => {
+          return pokemons.filter((pokemon) => {
+            return pokemon.weight < weight;
+          });
+        });
     }
   }
 
